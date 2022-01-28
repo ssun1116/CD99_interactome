a40e4f54b7ed7e381c8f1c5aa5b9be989a10baf0
Seoyeon Kim <syk991116@korea.ac.kr>
2022-01-28 18:03:01 +0900

    Update CD99_Localization.R

--- CD99_Localization.R
+++ CD99_Localization.R
@@ -1,7 +1,8 @@
-
-
-
+# Code for visualizing CD99 localization heatmap
 library(readxl)
+library(tidyverse)
+library(ggplot2)
+
 d = read_excel("~/Downloads/intensity.xlsx")
 d1 = d[1:11,]
 d1$Group = "WT"
@@ -14,7 +15,8 @@ dt = rbind.data.frame(d1, d2, d3)
 dt = dt[,c(5,2:4)]
 dt$name = paste(dt$Group, rownames(dt), sep = "_")
 colnames(dt) = c("Group", "cSMAC", "pSMAC", "dSMAC", "name")
-library(tidyverse)
+
+## Ordering data.frame
 dt.g = dt %>% gather("cSMAC":"dSMAC", key = "Group2", value = "value")
 dt.g$value = as.numeric(dt.g$value)
 dt.g$Group2 = factor(dt.g$Group2, levels=c("cSMAC","pSMAC","dSMAC"))
@@ -23,7 +25,7 @@ dt.g = dt.g[order(dt.g$Group),]
 dt.g$rank = as.numeric(rownames(dt.g))
 dt.g = dt.g[!is.na(dt.g$value),]
 
-library(ggplot2)
+## ggplot
 p <- ggplot(dt.g, aes(Group2 , reorder(name, desc(rank)))) +
   geom_tile(aes(fill = value), colour = "white", size = 0) + 
   theme_minimal()+
@@ -46,17 +48,10 @@ p <- ggplot(dt.g, aes(Group2 , reorder(name, desc(rank)))) +
 ggsave("~/Dropbox/NGR_SNU_2019/CD99_SYK_202201/Figure_localization_noline.pdf",p,  height = 4.5, width = 5.5)
 
 
-ggplot(dt.g, (aes(x = Group2, y =  reorder(name, desc(rank)), fill = value))) + 
-  geom_col(position = "fill", width = 1) + 
-  scale_fill_gradientn(colors = c("white", "#FFFFC6", "#EA3323"), values=rescale(c(min(dt.g[!is.na(dt.g$value),]$value), 600, max(dt.g[!is.na(dt.g$value),]$value))), 
-                       limits = c(0, 1600), oob=squish) +
-  facet_grid(Group ~ ., as.table = FALSE, switch = "y") 
-p
-
-
-
-
-  
-
+# ggplot(dt.g, (aes(x = Group2, y =  reorder(name, desc(rank)), fill = value))) + 
+#  geom_col(position = "fill", width = 1) + 
+#  scale_fill_gradientn(colors = c("white", "#FFFFC6", "#EA3323"), values=rescale(c(min(dt.g[!is.na(dt.g$value),]$value), 600, max(dt.g[!is.na(dt.g$value),]$value))), 
+#                       limits = c(0, 1600), oob=squish) +
+#  facet_grid(Group ~ ., as.table = FALSE, switch = "y") 
 
 
